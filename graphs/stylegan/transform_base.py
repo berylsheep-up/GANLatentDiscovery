d40a74c469011d23c97d2842fc4790154f461500
berylsheep-up <berylsheep@163.com>
2020-04-07 09:23:45 +0800

    constants

--- graphs/stylegan/transform_base.py
+++ graphs/stylegan/transform_base.py
@@ -1,12 +1,21 @@
+from . import constants
+
 class TransformGraph():
-    def __init__(self, walk_type, nsliders, eps, N_f, img_size, *args, **kwargs):
+    def __init__(self, walk_type, nsliders, eps, N_f, stylegan_opts, *args, **kwargs):
 
         # set class vars
         self.Nsliders = nsliders
         self.walk_type = walk_type
         self.N_f = N_f # NN num_steps
         self.eps = eps # NN step_size
-        self.img_size = img_size
+        self.dataset_args = constants.net_info[stylegan_opts.dataset]
+        self.img_size = self.dataset_args.img_size
+        self.dataset_name = stylegan_opts.dataset
+        self.latent = stylegan_opts.latent
+        if hasattr(stylegan_opts, 'truncation_psi'):
+            self.psi = stylegan_opts.truncation_psi
+        else:
+            self.psi = 1.0
 
     def vis_image_batch(self, graph_inputs, filename,
                         batch_start, wgt=False, wmask=False, num_panels=7):
